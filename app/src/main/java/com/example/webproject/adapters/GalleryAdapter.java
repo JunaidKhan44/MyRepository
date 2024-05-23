2b38eca2369df1258f2f3a1ec0da59aeefb54bc5
Junaid-Khan <80524662+JunaidKhan44@users.noreply.github.com>
2024-05-23 14:28:42 +0500

    Update GalleryAdapter.java

--- app/src/main/java/com/example/webproject/adapters/GalleryAdapter.java
+++ app/src/main/java/com/example/webproject/adapters/GalleryAdapter.java
@@ -41,17 +41,10 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         final GalleryModel files = filesList.get(position);
         final Uri uri = Uri.parse(files.getUri().toString());
         final File file = new File(uri.getPath());
-//         holder.userName.setText(files.getName());
 
         StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
         StrictMode.setVmPolicy(builder.build());
-
-//        if(files.getUri().toString().endsWith(".mp4"))
-//        {
-             holder.playIcon.setVisibility(View.VISIBLE);
-//        }else{
-//            holder.playIcon.setVisibility(View.INVISIBLE);
-//        }
+        holder.playIcon.setVisibility(View.VISIBLE);
         Glide.with(context)
                 .load(files.getUri())
                 .into(holder.savedImage);
@@ -72,39 +65,12 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
                         Toast.makeText(context, "No application found to open this file.", Toast.LENGTH_LONG).show();
                     }
                 }
-//                else if(files.getUri().toString().endsWith(".jpg")){
-//                    Uri VideoURI = FileProvider.getUriForFile(context, context.getApplicationContext().getPackageName() + ".provider",file);
-//                    Intent intent = new Intent();
-//                    intent.setAction(Intent.ACTION_VIEW);
-//                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                    intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-//                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-//                    intent.setDataAndType(VideoURI, "image/*");
-//                    try {
-//                        context.startActivity(intent);
-//                    } catch (ActivityNotFoundException e) {
-//                        Toast.makeText(context, "No application found to open this file.", Toast.LENGTH_LONG).show();
-//                    }
-//                }
             }
         });
         holder.repostID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Uri mainUri = Uri.fromFile(file);
-//                if(files.getUri().toString().endsWith(".jpg")){
-//                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-//                    sharingIntent.setType("image/*");
-//                    sharingIntent.putExtra(Intent.EXTRA_STREAM, mainUri);
-//                    sharingIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                    sharingIntent.setPackage("com.whatsapp");
-//                    try {
-//                        context.startActivity(Intent.createChooser(sharingIntent, "Share Image using"));
-//                    } catch (ActivityNotFoundException e) {
-//                        Toast.makeText(context, "No application found to open this file.", Toast.LENGTH_LONG).show();
-//                    }
-//                }
-              //  else
                     if(files.getUri().toString().endsWith(".mp4")){
                     Intent sharingIntent = new Intent(Intent.ACTION_SEND);
                     sharingIntent.setType("video/*");
@@ -119,78 +85,11 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
                 }
             }
         });
-//        holder.deleteID.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                final String path = filesList.get(position).getPath();
-//                final File file = new File(path);
-//                FancyAlertDialog.Builder alert = new FancyAlertDialog.Builder(context)
-//                        .setTextTitle("DELETE?")
-//                        .setBody("Are you sure you want to delete this file?")
-//                        .setNegativeColor(R.color.colorNegative)
-//                        .setNegativeButtonText("Cancel")
-//                        .setOnNegativeClicked(new FancyAlertDialog.OnNegativeClicked() {
-//                            @Override
-//                            public void OnClick(View view, Dialog dialog) {
-//                                dialog.dismiss();
-//                            }
-//                        })
-//                        .setPositiveButtonText("Delete")
-//                        .setPositiveColor(R.color.colorPositive)
-//                        .setOnPositiveClicked(new FancyAlertDialog.OnPositiveClicked() {
-//                            @Override
-//                            public void OnClick(View view, Dialog dialog) {
-//                                try {
-//                                    if (file.exists()) {
-//                                        boolean del = file.delete();
-//                                        filesList.remove(position);
-//                                        notifyItemRemoved(position);
-//                                        notifyItemRangeChanged(position, filesList.size());
-//                                        notifyDataSetChanged();
-//                                        Toast.makeText(context, "File was Deleted", Toast.LENGTH_SHORT).show();
-//                                        if(del){
-//                                            MediaScannerConnection.scanFile(
-//                                                    context,
-//                                                    new String[]{ path, path},
-//                                                    new String[]{ "image/jpg","video/mp4"},
-//                                                    new MediaScannerConnection.MediaScannerConnectionClient()
-//                                                    {
-//                                                        public void onMediaScannerConnected()
-//                                                        {
-//                                                        }
-//                                                        public void onScanCompleted(String path, Uri uri)
-//                                                        {
-//                                                            Log.d("Video path: ",path);
-//                                                        }
-//                                                    });
-//                                        }
-//                                    }
-//                                    dialog.dismiss();
-//                                } catch (Exception e) {
-//                                    // TODO let the user know the file couldn't be deleted
-//                                    e.printStackTrace();
-//                                }
-//                            }
-//                        })
-//                        .build();
-//                alert.show();
-//            }
-//        });
+
         holder.shareID.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Uri mainUri = Uri.fromFile(file);
-//                if(files.getUri().toString().endsWith(".jpg")){
-//                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-//                    sharingIntent.setType("image/*");
-//                    sharingIntent.putExtra(Intent.EXTRA_STREAM, mainUri);
-//                    sharingIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-//                    try {
-//                        context.startActivity(Intent.createChooser(sharingIntent, "Share Image using"));
-//                    } catch (ActivityNotFoundException e) {
-//                        Toast.makeText(context, "No application found to open this file.", Toast.LENGTH_LONG).show();
-//                    }
-//                }else
                    if(files.getUri().toString().endsWith(".mp4")){
                     Intent sharingIntent = new Intent(Intent.ACTION_SEND);
                     sharingIntent.setType("video/*");
@@ -220,7 +119,6 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
         ImageView repostID, shareID, deleteID;
         public ViewHolder(View itemView) {
             super(itemView);
-           // userName = (TextView) itemView.findViewById(R.id.profileUserName);
             savedImage = (ImageView) itemView.findViewById(R.id.mainImageView);
             playIcon = (ImageView) itemView.findViewById(R.id.playButtonImage);
             repostID = (ImageView) itemView.findViewById(R.id.repostID);
